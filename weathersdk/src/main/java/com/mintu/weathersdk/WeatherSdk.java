d7ef49a9c806b868685264bc3647ef2da1bf75b9
mintu <mintu.giri1997@gmail.com>
2020-09-26 17:45:28 +0530

    new

--- weathersdk/src/main/java/com/mintu/weathersdk/WeatherSdk.java
+++ weathersdk/src/main/java/com/mintu/weathersdk/WeatherSdk.java
@@ -3,7 +3,6 @@ package com.mintu.weathersdk;
 import android.app.ProgressDialog;
 import android.content.Context;
 import android.util.Log;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import com.android.volley.Request;
